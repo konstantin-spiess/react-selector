6756e3c2140e0f08274a688fb2d5c1d9eec60e05
Konstantin Spieß <mail@konstantinspiess.com>
2022-07-09 20:05:42 +0200

    added content script

--- src/devtools/devtools.ts
+++ src/devtools/devtools.ts
@@ -1,8 +1,8 @@
 export {};
 
 chrome.devtools.panels.elements.createSidebarPane('React Selector', (panel) => {
-  // chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
-  //   chrome.devtools.inspectedWindow.eval('parseDOM($0)', { useContentScriptContext: true });
-  // });
+  chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
+    chrome.devtools.inspectedWindow.eval('parseDOM($0)', { useContentScriptContext: true });
+  });
   panel.setPage('src/devtools/panel/panel.html');
 });
