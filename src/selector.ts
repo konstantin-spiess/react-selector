111acbe91dd255a9f6ed0314c9c773f8d45e16ff
Konstantin Spieß <mail@konstantinspiess.com>
2022-08-05 17:35:41 +0200

    added bfs for react root

--- src/selector.ts
+++ src/selector.ts
@@ -1,27 +1,37 @@
+import { findReactRoot } from './utils/domUtils';
 import { getReactComponentName, getReactFiber, isReactFiberRootNode } from './utils/reactUtils';
 
 const selectedElemetMarkerQuery = import.meta.env.VITE_SELECTED_ELEMENT_MARKER_QUERY;
 
-document.addEventListener('changeSelection', () => {
-  const selectedElement = document.querySelector(selectedElemetMarkerQuery);
-  if (!selectedElement) return;
+init();
 
-  const fibernode = getReactFiber(selectedElement);
+function init() {
+  const reactRootElement = findReactRoot(document.body);
+  if (!reactRootElement) {
+    return;
+  }
 
-  if (!fibernode) {
-    console.log('not part of react application');
-    return;
-  }
-  if (isReactFiberRootNode(fibernode)) {
-    console.log('React root');
-    return;
-  }
-  const name = getReactComponentName(fibernode);
-  if (!name) {
-    console.log('no react component');
-    return;
-  }
-  console.log(name);
-});
+  document.addEventListener('changeSelection', () => {
+    const selectedElement = document.querySelector(selectedElemetMarkerQuery);
+    if (!selectedElement) return;
+
+    const fibernode = getReactFiber(selectedElement);
+
+    if (!fibernode) {
+      console.log('not part of react application');
+      return;
+    }
+    if (isReactFiberRootNode(fibernode)) {
+      console.log('React root');
+      return;
+    }
+    const name = getReactComponentName(fibernode);
+    if (!name) {
+      console.log('no react component');
+      return;
+    }
+    console.log(name);
+  });
+}
 
 export {};
--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -1,1 +1,22 @@
+import { isReactRoot } from './reactUtils';
+
+/**
+ * Find the react root element with bfs
+ * @param startElement element that contains the react root element
+ * @returns the react root element or null
+ */
+export function findReactRoot(startElement: HTMLElement): HTMLElement | null {
+  const queue = [startElement];
+  while (queue.length > 0) {
+    const currentElement = queue.shift()!;
+    if (isReactRoot(currentElement)) {
+      return currentElement;
+    }
+    for (const child of currentElement.children) {
+      queue.push(child as HTMLElement);
+    }
+  }
+  return null;
+}
+
 export {};
