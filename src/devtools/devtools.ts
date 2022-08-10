433c890bec6e9a6b68c37a556975689dc8b5d8b0
Konstantin Spieß <mail@konstantinspiess.com>
2022-08-10 16:37:21 +0200

    message passing selector to devtools panel react application

--- src/devtools/devtools.ts
+++ src/devtools/devtools.ts
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { ChangeSelectionMessage, isSelectorMessage } from '../types/message';
+import { ChangeSelectionMessage, isInitMessage, isMessage, Message } from '../types/message';
 
 //
 // Message passing: devtools -> background
@@ -42,12 +42,59 @@ chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
 //
 chrome.devtools.panels.elements.createSidebarPane('React Selector', (panel) => {
   panel.setPage('src/devtools/panel/panel.html');
+});
 
-  backgroundConnection.onMessage.addListener((message) => {
-    if (isSelectorMessage(message)) {
-      panel.setObject(message.selector);
+let connections: { [key: number]: chrome.runtime.Port } = {};
+
+// Manage incomming connections only from devtools
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name !== 'panel') {
+    return;
+  }
+
+  // Handle different messages in listener
+  const messageListener = (message: Message) => {
+    if (!isMessage(message)) {
+      return;
+    }
+    if (isInitMessage(message)) {
+      connections[message.tabId] = port;
+      return;
+    }
+  };
+
+  // Attach listener to devtools connection
+  port.onMessage.addListener(messageListener);
+
+  // Handle disconnect
+  port.onDisconnect.addListener((port) => {
+    port.onMessage.removeListener(messageListener);
+
+    const tabs = Object.keys(connections);
+    for (let i = 0, len = tabs.length; i < len; i++) {
+      if (connections[Number(tabs[i])] == port) {
+        delete connections[Number(tabs[i])];
+        break;
+      }
     }
   });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // Messages from content scripts should have sender.tab set
+  if (sender.tab) {
+    const tabId = sender.tab.id;
+    if (!tabId) {
+      console.log('tabId is undefined');
+    } else if (tabId in connections) {
+      connections[tabId].postMessage(request);
+    } else {
+      console.log('Tab not found in connection list.');
+    }
+  } else {
+    console.log('sender.tab not defined.');
+  }
+  return true;
 });
 
 export {};
